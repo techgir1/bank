014176dbdcf549cb2e8fbb90225dc5cc5a2a9f8e
Beth Fostergit config --global user.namegit config --global user.name Beth <beth@techgir1.com>
2016-01-26 13:22:12 -0800

    added logic to new account form

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,14 @@ function bankAccount(memberName,initialDeposit) {
 bankAccount.prototype.depositWithdraw = function(deposit, withdraw) {
 	this.initialDeposit = this.initialDeposit + deposit - withdraw;
 }
+
+$(document).ready(function() {
+	var newAccount;
+	$("form#new-account").submit(function(event) {
+		var userName = $("input#new-user-name").val();
+		var initialBalance = parseInt($("input#initial-balance").val());
+		newAccount = new bankAccount(userName, initialBalance);
+		$("span.balance").text(newAccount.initialDeposit);
+		event.preventDefault();
+	});
+});
